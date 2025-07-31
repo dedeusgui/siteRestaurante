5e9990d01f5f74731fa0f08d6834df82fdf62ef1
dedeusgui <guilherme.dalosto@aluno.senai.br>
2025-07-31 17:14:42 -0300

    melhoria na estética e funcionamento do site

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ function valorPedido() {
 
   total = parseInt(precoPrato) + parseInt(precoBebida);
   document.getElementById("total").innerHTML = "Total: R$ " + total;
+  document.getElementById("totalModal").innerHTML = "R$ " + total;
 }
+window.addEventListener("load", valorPedido);
+window.addEventListener("change", valorPedido);
+
+function gerarQrCodePix(pixPayload) {
+  try {
+    const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${pixPayload}`;
+    const qrCodeContainer = document.querySelector(".qr-code");
+    qrCodeContainer.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = apiUrl;
+    img.alt = "QR Code para pagamento Pix";
+
+    qrCodeContainer.appendChild(img);
+  } catch (error) {
+    console.error("Ocorreu um erro ao gerar o QR Code:", error);
+  }
+}
+
+const qrCodeModal = document.getElementById("qrCodeModal");
+qrCodeModal.addEventListener("show.bs.modal", function (event) {
+  gerarQrCodePix(2199751684);
+});
